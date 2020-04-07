cc73bd812f0d4781f88e5bcf3aad9621a20b5a2a
Michel França <fr.mich17@gmail.com>
2020-04-06 21:16:19 -0300

    styles

---
+++ script.js
@@ -0,0 +1,8 @@
+document
+    .querySelector('button.seeIdea')
+    .addEventListener('click', () => {
+        document
+            .querySelector('#footer')
+            .classList
+            .toggle('hide')
+    })
